26f0c52e53a2862250d60a398a8474de129c88b9
shaun <shaun965@hotmail.com>
2016-12-01 20:56:10 -0800

    updated product controller to take in image_url

--- app/controllers/api/v1/product_controller.rb
+++ app/controllers/api/v1/product_controller.rb
@@ -9,18 +9,24 @@ class Api::V1::ProductController < Api::V1::BaseController
   end
 
   def create
-    base_64_image = params[:image]
-    blob = Base64.decode64(base_64_image)
-    decoded_image = MiniMagick::Image.read(blob)
-    image_file = File.open(decoded_image.path)
-    @product = Product.new(product_params.merge(:image=> image_file))
+    if params[:image_url]
+      @product = Product.new(product_params.merge(:image_url=> params[:image_url]))
+    else
+      base_64_image = params[:image]
+      blob = Base64.decode64(base_64_image)
+      decoded_image = MiniMagick::Image.read(blob)
+      image_file = File.open(decoded_image.path)
+      @product = Product.new(product_params.merge(:image=> image_file))
+    end
     @category = Category.find_by(name: params[:category])
     @product.category = @category
     @product.store = current_user.store
     @product.user = current_user
     if @product.save
-      @product.image_url = @product.image.url
-      @product.save
+      if @product.image_url.nil? 
+        @product.image_url = @product.image.url
+        @product.save
+      end
       success_response 201, "Product created successfully", product: @product
     else
       error_response 422, "Product could not be created", @product.errors.full_messages
